b85b3fb0b63d534012bde5aa78e40fb3a8e783e9
fauzia12345 <110169642+fauzia12345@users.noreply.github.com>
2022-08-22 09:27:06 +0000

    Add files via upload

---
+++ datasource/products.js
@@ -0,0 +1,24 @@
+const products=[
+    {
+name:"Tomatoes",
+prices:25,
+owner:"Latif",
+qnty:2.5,
+image:"tomatoes.jpeg",
+description:"it is foreign tomatoes from Brazil",
+id:"fc1004@25",
+isNew:true
+    },
+ {  
+name:"soyabeans",
+prices:400,
+owner:"Desmond",
+qnty:7,
+image:"soyabeans.jpeg",
+description:"Brifor first class soyabeans",
+id:"fc1024@22",
+isNew:false,
+    },
+];
+
+module.exports=products
